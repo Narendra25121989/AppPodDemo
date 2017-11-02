ff20b56381aae1cf7cf87765d1c4ffa831966c46
Narendra <narendrasuthar23@gmail.com>
2017-11-02 15:10:28 +0530

    init

---
+++ Desktop/New/Pod/PodCode/PodCode/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  PodCode
+//
+//  Created by MAC3 on 11/2/17.
+//  Copyright © 2017 app. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func ViewInit() -> Void {
+        print("Narendra")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
